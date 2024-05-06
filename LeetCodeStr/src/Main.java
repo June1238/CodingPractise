3c13ad296ae42f91dcc81a8c876823ae7ab445fb
June1238 <june15610531862@163.com>
2024-05-06 21:44:47 +0800

    回文字符串：回溯+dp预处理 + plus 寻找最少次数[2*dp]

--- LeetCodeStr/src/Main.java
+++ LeetCodeStr/src/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         partitions("aabaa");
+        System.out.println(minCut("aab"));
     }
 
     /**分割回文串：给定字符串s 将s分割成一些子串 使每个子串都是回文串 返回s
@@ -62,5 +63,72 @@ public class Main {
                 temp.removeLast();
             }
         }
+    }
+
+    /**
+     * 分割回文串 - 给定一个字符串s 将s分割成一些子串 使每个子串都是回文串
+     * 返回符合要求的最小分割次数*/
+    public static int minCut(String s){
+        int cnt = 0;
+        int len = s.length();
+        /**预处理字符串 制定好分割点*/
+        boolean[][] dp = new boolean[len+1][len+1];
+        for(int i = 0;i<=len;i++){
+            Arrays.fill(dp[i],false);
+        }
+        for(int i = 0;i<len;i++){
+            dp[i][i] = true;
+            for(int j = 0;j<len&&j!=i;j++){
+                if(s.charAt(i)==s.charAt(j)&&(Math.abs(i-j)==1||dp[Math.max(i,j)-1][Math.min(i,j)+1]))
+                    dp[i][j] = dp[j][i] = true;
+            }
+        }
+
+        int[] res = new int[len+1];
+        res[0] = 0;
+        for(int i = 0;i<len;i++){
+            res[i+1] = res[i]+1;
+            for(int j = 0;j<i;j++){
+                if(dp[i][j]==true)
+                    res[i+1] = Math.min(res[i+1],res[j]+1);
+            }
+        }
+        return res[len]-1;
+    }
+}
+
+/**standard answer*/
+class Solution{
+    public int minCnt(String s){
+        int n = s.length();
+        boolean[][] g = new boolean[n][n];
+        for(int i = 0;i<n;i++){
+            Arrays.fill(g[i],true);
+        }
+
+        /** 要求是在进行计算机的时候 前半部分的值都已经转移过 即使用过了
+         * 判断是否为回文串的方法-思路：常规的方法是使用双指针分别指向i和j 每次
+         * 判断两个指针指向的字符是否相同 直到两个指针相遇 然而这种方法会产生
+         * 重复计算
+         * */
+        for(int i = n-1;i>=0;i--){
+            for(int j = i+1;j<n;j++){
+                g[i][j] = s.charAt(i)==s.charAt(j)&&g[i+1][j-1];
+            }
+        }
+
+        int[] f = new int[n];
+        Arrays.fill(f,Integer.MAX_VALUE);
+        for(int i = 0;i < n;i ++){
+            /**截止到本节点为止的最小分割次数*/
+            if(g[0][i]) f[i] = 0;
+            else{
+                for(int j = 0;j<i;j++){
+                    if(g[j+1][i])
+                        f[i] = Math.min(f[i],f[j]+1);
+                }
+            }
+        }
+        return f[n-1];
     }
 }
