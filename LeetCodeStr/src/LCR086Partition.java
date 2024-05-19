b017e7fe755d57d0d712874c95783fe4f5231c2b
June1238 <june15610531862@163.com>
2024-05-19 16:47:04 +0800

    519LeetCode

--- LeetCodeStr/src/LCR086Partition.java
+++ LeetCodeStr/src/LCR086Partition.java
@@ -3,12 +3,11 @@ import java.util.*;
 public class LCR086Partition {
 
     public static void main(String[] args){
-        System.out.println(findMinimumTimeI(new int[][]{{10,18,2},{1,8,1},{10,20,8}}));
-        System.out.println(findMinimumTimeI(new int[][]{{2,3,1},{4,5,1},{1,5,2}}));
-        System.out.println(findMinimumTimeI(new int[][]{{1,3,2},{2,5,3},{5,6,2}}));
-        System.out.println(findMinimumTimeI(new int[][]{{8,19,1},{3,20,1},{1,20,2},{6,13,3}}));
-        System.out.println(findMinimumTimeI(new int[][]{{1,10,7},{4,11,1},{3,19,7},{10,15,2}}));
-        System.out.println(findMinimumTimeI(new int[][]{{1,14,4},{4,20,3},{17,19,2},{1,16,3}}));
+        SolutionA solutionA = new SolutionA();
+        System.out.println(solutionA.getWinnerI(new int[]{2,1,3,5,4,6,7},2));
+        System.out.println(solutionA.getWinnerI(new int[]{3,2,1},10));
+        System.out.println(solutionA.getWinner(new int[]{1,9,8,2,3,7,6,4,5},7));
+        System.out.println(solutionA.getWinner(new int[]{1,11,22,33,44,55,66,77,88,99},1000000000));
     }
 
     /**
@@ -116,5 +115,88 @@ class SolutionA{
         }
         return right;
     }
-}
 
+    /**1535 找出游戏中的赢家
+     * 给出一个由不同整数组成的整数数组arr和一个整数k 每回合游戏都在数组中的
+     * 前两个元素[arr[0]和arr[1]]之间进行 比较两者大小 较大的整数将会取
+     * 得这一回合的胜利并保留在位置0 较小的整数移至数组末尾 当一个整数连续赢得
+     * k个连续回合时 游戏结束 该整数就是比赛的赢家
+     *
+     * 链表+模拟
+     * */
+    public int getWinner(int[] arr,int k){
+        int res = 0;
+        if(arr.length==1) return arr[0];
+        if(arr.length<=2) return Math.max(arr[0],arr[1]);
+        LinkNode head = new LinkNode(arr[0]);
+        LinkNode tail = new LinkNode(),ptr = head;
+        for(int i = 0;i<arr.length - 1;i++){
+            ptr.next = new LinkNode(arr[i+1]);
+            ptr = ptr.next;
+            tail = ptr;
+        }
+        /**arr中的整数各不相同*/
+        int cur = 0;
+        while(cur<k){
+            LinkNode nPtr = head.next;
+            if(head.getVal()>nPtr.getVal()){
+                head.next = nPtr.next;
+                tail.next = nPtr;
+                nPtr.next = null;
+                tail = nPtr;
+            }else{
+                tail.next = head;
+                head.next = null;
+                tail = head;
+                head = nPtr;
+            }
+            if(head.getVal()==res){
+                cur++;
+            }
+            else {
+                res = head.getVal();
+                cur = 1;
+            }
+        }
+        return res;
+    }
+
+    /**找出数组游戏的赢家 给你一个有不同的整数的数组arr和一个整数k
+     * 比较arr[0]和arr[1]的位置
+     * */
+    public int getWinnerI(int[] arr,int k){
+        int maxNum = arr[0];
+        for(int num : arr){ maxNum = Math.max(maxNum,num);}
+        int cur = 0 , res = arr[0];
+        for(int i = 1;i<arr.length;i++){
+            if(arr[i] > res)
+            {
+                res = arr[i];
+                cur = 1;
+            }
+            else {
+                cur++;
+            }
+            if(cur==k) return res;
+            if(arr[i] == maxNum) return arr[i];
+        }
+        return maxNum;
+    }
+
+}
+class LinkNode{
+    int val;
+    LinkNode next;
+    public LinkNode(){}
+    public LinkNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+    public LinkNode(int val,LinkNode next){
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal(){return this.val;}
+    public LinkNode getNext(){ return this.next;}
+}
